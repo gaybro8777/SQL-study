728b1f0fb207c2ea05613bbf3bc060d1ea6b9312
dywangk <sexgender@163.com>
2022-02-13 21:57:13 +0800

    删除冗余文件

--- Oracle/Oracle11gR2密码过期处理.sql
+++
@@ -1,9 +0,0 @@
---查询密码期限
-SELECT * FROM dba_profiles s WHERE s.profile='DEFAULT' AND resource_name='PASSWORD_LIFE_TIME'
---修改密码期限为无限制
-ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED; 
-
-
-
-
-
